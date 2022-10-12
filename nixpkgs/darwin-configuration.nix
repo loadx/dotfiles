e71412b81f9599b4d0d84056692b60464e665354
Mat Brennan <mat.brennan@amber.com.au>
2022-10-12 23:29:10 +1100

    Updates

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -1,21 +1,16 @@
-{ config, pkgs, lib, options, ... }:
+{ config, pkgs, ... }:
 
-with pkgs.stdenv; with lib; {
-  imports = [
-    <home-manager/nix-darwin>
-    #~/.config/nixpkgs/link-apps
+{
+  imports = [ 
+    #<home-manager/nix-darwin>
+    #./home.nix 
   ];
 
-  nixpkgs = {
-    config.allowUnfree = true;
-  };
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = [ 
-    pkgs.vim
-    pkgs.direnv
-    pkgs.nixpkgs-fmt
+  environment.systemPackages = with pkgs; [
+    vim
+    fd
+    htop
+    watch
   ];
 
   # Use a custom configuration.nix location.
@@ -23,75 +18,47 @@ with pkgs.stdenv; with lib; {
   environment.darwinConfig = "$HOME/.config/nixpkgs/darwin-configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
-  # nix.package = pkgs
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
+  # lorri
+  services.lorri.enable = true;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
-  programs.zsh.enableCompletion = false;
-
-  # auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-
-  users.users.loadx = {
-    name = "Mat Brennan";
-    home = "/Users/loadx";
-    shell = pkgs.zsh; 
-  };
-
-  home-manager.users.loadx = import ~/.config/nixpkgs/home.nix;
-  home-manager.useGlobalPkgs = true;
-  services.lorri.enable = true;
-  
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
-
-  # mac settings
-  system.defaults.dock = {
-    autohide = true;
-    autohide-delay = "0";
-    mineffect = "scale";
-    orientation = "bottom";
-    tilesize = 32;
-    expose-group-by-app = false;
-  };
-
-  system.defaults.NSGlobalDomain = {
-    AppleInterfaceStyle = "Dark";
-    "com.apple.swipescrolldirection" = false;
-    "com.apple.mouse.tapBehavior" = 1;
-    "com.apple.trackpad.scaling" = "2.8";
-  };
-
-  system.defaults.trackpad = {
-    TrackpadRightClick = true;
-  };
-
-  #system.defaults.NSGlobalDomain.InitialKeyRepeat = 5;
-  #system.defaults.NSGlobalDomain.KeyRepeat = 5;
-  system.defaults.NSGlobalDomain.AppleShowScrollBars = "Always";
-  system.defaults.spaces.spans-displays = true;
 
   homebrew = {
     enable = true;
     brewPrefix = "/opt/homebrew/bin";
-    taps = [
-      "homebrew/cask-fonts"
-      "homebrew/cask-drivers"
-    ];
+    onActivation = {
+      cleanup = "uninstall";
+      upgrade = true;
+    };
+
     casks = [
-      "alt-tab"
-      "karabiner-elements"
-      "rectangle"
-      "kitty"
-      "DisplayLink"
-      "iterm2"
-      "visual-studio-code"
-      "pop"
+      "docker"
       "microsoft-remote-desktop"
+      "visual-studio-code"
+      "kitty"
+      "font-sauce-code-pro-nerd-font"
+      "displaylink"
+      "pop"
+      "zoom"
+      "aws-vpn-client"
+      "rectangle"
+      "alt-tab"
+      "postman"
     ];
-    brews = [
-    ];
+
+    brews = [];
+
+    taps = [
+    "homebrew/cask-fonts"
+    "homebrew/cask-drivers"
+    ];    
   };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 }
--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,5 +1,11 @@
 { config, pkgs, lib, ... }:
-{
+let
+/*   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  master = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
+    sha256 = "1s2vy5n2pa7j5zbrzg757hrxccld43pv2lab7x16qnr6d11rw27v";
+  }) {}; */
+in {
   home.stateVersion = "22.05";
 
   # Home Manager needs a bit of information about you and the
@@ -20,22 +26,27 @@
       ls = "ls --color";
       ll = "ls -la";
       grep = "grep --color=auto --exclude-dir=.csv --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn";
+      check_staging = "(cd /Users/loadx/src/amber/amber-core/packages/admin-cli && aws-vault exec amber-staging -- npm run start -- staging show)";
+      take_staging = "(cd /Users/loadx/src/amber/amber-core/packages/admin-cli && aws-vault exec amber-staging -- npm run start -- staging take -e mat.brennan@amber.com.au)";
+      release_staging = "(cd /Users/loadx/src/amber/amber-core/packages/admin-cli && aws-vault exec amber-staging -- npm run start -- staging release -e mat.brennan@amber.com.au)";
+      admin-cli = "cd /Users/loadx/src/amber/amber-core/packages/admin-cli";
     };
     initExtraBeforeCompInit = ''
       #when debugging -> zmodload zsh/zprof
       bindkey  "^[[3~"  delete-char
-      source ~/.config/nixpkgs/p10k-config/p10k.zsh
+      source ~/.config/p10k-config/p10k.zsh
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     '';
     initExtra = ''
+      setopt appendhistory
+      setopt INC_APPEND_HISTORY  
+      setopt SHARE_HISTORY
+      export SAVEHIST=10000 
+      export HISTFILE=~/.zsh_history
       source /etc/zshrc.local
       export TERM=xterm-256color
       eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
-    history = {
-      size = 10000;
-      path = "~/.zsh_history";
-    };
     zplug = {
       enable = true;
 
@@ -51,7 +62,8 @@
   programs.git = {
     enable = true;
     userName = "Mat Brennan";
-    userEmail = "loadx.mat@gmail.com";
+    userEmail = "mat.brennan@amber.com.au";
+    #package = master.git;
     aliases = {
       ll = ''log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat --date=relative'';
       lsd = ''log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short'';
@@ -116,64 +128,65 @@
   programs.kitty = {
     enable = true;
     extraConfig = ''
-			font_size 13.5
-      font_family "SauceCodePro Nerd Font Mono Regular"
-			cursor #cccccc
-			cursor_shape block
-			cursor_stop_blinking_after 5.0
-			url_color #0087bd
-			url_style curly
-			open_url_with default
-			detect_urls yes
-			copy_on_select clipboard
-			mouse_map middle release ungrabbed paste_from_selection
-			mouse_map left press ungrabbed mouse_selection normal
-			mouse_map alt+left press ungrabbed mouse_selection rectangle
-			mouse_map left doublepress ungrabbed mouse_selection word
-			mouse_map left triplepress ungrabbed mouse_selection line
-			mouse_map shift+left press ungrabbed,grabbed mouse_selection normal
-			enable_audio_bell no
-			draw_minimal_borders yes
-			window_margin_width 0
-			window_padding_width 1
+	font_size 13.5
+      	font_family "SauceCodePro Nerd Font Mono Regular"
+	cursor #cccccc
+	cursor_shape block
+	cursor_stop_blinking_after 5.0
+	url_color #0087bd
+	url_style curly
+	open_url_with default
+	detect_urls yes
+	copy_on_select clipboard
+	mouse_map middle release ungrabbed paste_from_selection
+	mouse_map left press ungrabbed mouse_selection normal
+	mouse_map alt+left press ungrabbed mouse_selection rectangle
+	mouse_map left doublepress ungrabbed mouse_selection word
+	mouse_map left triplepress ungrabbed mouse_selection line
+	mouse_map shift+left press ungrabbed,grabbed mouse_selection normal
+	enable_audio_bell no
+	draw_minimal_borders yes
+	window_margin_width 0
+	window_padding_width 1
 
-			# Base16 Harmonic16 Dark - kitty color config
-			# Scheme by Jannik Siebert (https://github.com/janniks)
-			background #0b1c2c
-			foreground #cbd6e2
-			selection_background #cbd6e2
-			selection_foreground #0b1c2c
-			url_color #aabcce
-			cursor #cbd6e2
-			active_border_color #627e99
-			inactive_border_color #223b54
-			active_tab_background #0b1c2c
-			active_tab_foreground #cbd6e2
-			inactive_tab_background #223b54
-			inactive_tab_foreground #aabcce
-			tab_bar_background #223b54
-			# normal
-			color0 #0b1c2c
-			color1 #bf8b56
-			color2 #56bf8b
-			color3 #8bbf56
-			color4 #8b56bf
-			color5 #bf568b
-			color6 #568bbf
-			color7 #cbd6e2
+	# Base16 Harmonic16 Dark - kitty color config
+	# Scheme by Jannik Siebert (https://github.com/janniks)
+	background #0b1c2c
+	foreground #cbd6e2
+	selection_background #cbd6e2
+	selection_foreground #0b1c2c
+	url_color #aabcce
+	cursor #cbd6e2
+	active_border_color #627e99
+	inactive_border_color #223b54
+	active_tab_background #0b1c2c
+	active_tab_foreground #cbd6e2
+	inactive_tab_background #223b54
+	inactive_tab_foreground #aabcce
+	tab_bar_background #223b54
 
-			# bright
-			color8 #627e99
-			color9 #bfbf56
-			color10 #223b54
-			color11 #405c79
-			color12 #aabcce
-			color13 #e5ebf1
-			color14 #bf5656
-			color15 #f7f9fb
+	# normal
+	color0 #0b1c2c
+	color1 #bf8b56
+	color2 #56bf8b
+	color3 #8bbf56
+	color4 #8b56bf
+	color5 #bf568b
+	color6 #568bbf
+	color7 #cbd6e2
 
-                        background_opacity .97
-			dynamic_background_opacity no
+	# bright
+	color8 #627e99
+	color9 #bfbf56
+	color10 #223b54
+	color11 #405c79
+	color12 #aabcce
+	color13 #e5ebf1
+	color14 #bf5656
+	color15 #f7f9fb
+
+        background_opacity .97
+	dynamic_background_opacity no
     '';
   };
 
@@ -187,11 +200,14 @@
     curl
     wget
     jq
-    #niv
-    git
+    niv
     htop
     grc
     keychain
+    awscli2
+    fd
+    watch
+    tree
   ] ++ lib.optionals stdenv.isDarwin [
     # mac only things
     m-cli
